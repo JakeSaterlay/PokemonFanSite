3e4b7119d14066bd93b63bb3291faae6257f4da7
Jake Saterlay <jake@vocala.co>
2020-09-19 02:27:04 +0100

    added button to link to region list page

---
+++ PokemonFanSite/Pages/Regions/RegionList.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PokemonFanSite.Pages.Regions
+{
+    public class RegionListModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
--- PokemonFanSite/wwwroot/js/home-page.js
+++ PokemonFanSite/wwwroot/js/home-page.js
@@ -1,5 +1,5 @@
 ﻿$(document).ready(function () {
 
     $(".main-welcome").fadeIn(2000);
-
+    $(".enter-button").fadeIn(4000);
 });
